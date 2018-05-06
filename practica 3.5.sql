712679000e9b2d9d3cb27eef245eacb6acf7fb57
AaronEz <31638321+AaronEz@users.noreply.github.com>
2018-05-05 19:17:42 -0500

    practica 3.5

---
+++ practica 3.5.sql
@@ -0,0 +1,14 @@
+select * from Alumno
+select * from Salon
+select * from Maestro
+
+select * from Grupo
+
+select * from GrupoAlumno
+
+
+select * from Carrera
+select * from Materia
+
+select * from HorarioAsignado
+select * from Horario
