1d0c7e1f2488be71382cee1a1d4bac8beb99983b
AaronEz <31638321+AaronEz@users.noreply.github.com>
2018-05-05 19:22:27 -0500

    Practica 6 y 7

--- Practica 6 y 7.sql
+++ Practica 6 y 7.sql
@@ -1,4 +1,4 @@
---select simple
+--tablas llenas
 select * from Alumno
 select * from Carrera
 select * from Maestro
@@ -9,63 +9,38 @@ select * from Grupo
 select * from HorarioAsignado
 select * from GrupoAlumno
 
--- select group by
+--tablas restantes
 
-SELECT Nombre ='tilo'
-FROM Maestro
-GROUP BY Nombre;
+select GrupoAlumno.IdAlumno, HorarioAsignado.IdGrupo, HorarioAsignado.IdHorario
+from GrupoAlumno
+inner join HorarioAsignado on GrupoAlumno.IdGrupo = HorarioAsignado.IdGrupo;
 
-SELECT IdHorario FROM Horario
-WHERE Turno=2
-GROUP BY IdHorario;
+select Carrera.Nombre, Materia.Nombre 
+from Materia
+inner join Carrera on Materia.IdCarrera = Carrera.IdCarrera;
 
-SELECT IdAlumno FROM Alumno
-WHERE Nombre='BETY'
-GROUP BY IdAlumno;
+select Maestro.Nombre, Materia.Nombre 
+from Maestro
+inner join Materia on Maestro.IdMateria = Materia.IdMateria;
 
-SELECT IdAlumno FROM Alumno
-WHERE Nombre='JOSUE'
-GROUP BY IdAlumno;
+select Carrera.Nombre as Carrera , Materia.Nombre as Materia, Maestro.Nombre as Maestro
+from Materia
+inner join Carrera on Materia.IdCarrera = Carrera.IdCarrera inner join Maestro on Maestro.IdMateria = Materia.IdMateria;
 
-SELECT Nombre FROM Carrera
-WHERE Duracion=4
-GROUP BY Nombre;
+select HorarioAsignado.IdGrupo, Horario.DIa, Horario.HorarioInicio, Horario.HorarioFin, Horario.Turno
+from Horario 
+inner join HorarioAsignado on Horario.IdHorario = HorarioAsignado.Idhorario;
 
---select funciones agregadas
-declare @Alumnos int = (select COUNT(*) as Alumnos from Alumno)
-declare @Maestros int = (select COUNT(*) as Mastros from Maestro)
-declare @HorasxSemana int =(select COUNT(*) as HorasxSemana from Horario)
-declare @Materias int = (select count(*) as Materias from Materia)
-declare @Salones int = (select count (*) as Salones from Salon)
+select GrupoAlumno.IdAlumno, GrupoAlumno.IdGrupo, Alumno.Nombre, HorarioAsignado.IdGrupo
+from GrupoAlumno
+inner join HorarioAsignado on HorarioAsignado.IdGrupo=HorarioAsignado.IdGrupo  inner join Alumno on GrupoAlumno.IdAlumno = Alumno.IdAlumno
 
-begin
-select sum (@Alumnos + @Maestros ) as REGISTRADOS
-end
+select GrupoAlumno.IdAlumno, GrupoAlumno.IdGrupo, Alumno.Nombre, HorarioAsignado.IdGrupo
+from GrupoAlumno 
+inner join HorarioAsignado on HorarioAsignado.IdGrupo=GrupoAlumno.IdGrupo  inner join Alumno on GrupoAlumno.IdAlumno = Alumno.IdAlumno 
 
-begin
-select sum (@HorasxSemana / @Salones) as HorasDiarias
-end
 
-begin 
-select sum (@Alumnos /  @Salones) as AlumnosxSalon
-end
 
-begin
-select sum(@HorasxSemana / @Materias ) as FrecuenciaxSemana
-end
 
-begin
-select sum(@HorasxSemana / @Maestros) as FrecuenciaxSemanaMaestros
-end
 
---select top
 
-SELECT TOP 3 * FROM Alumno;
-
-SELECT TOP 5 * FROM Grupo;
-
-SELECT TOP 6 * FROM Maestro;
-
-SELECT TOP 5 * FROM Horario;
-
-SELECT TOP 3 * FROM Materia;
